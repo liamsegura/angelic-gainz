5a14542862b2cb4f7b350b9e9fad3541d98ad35d
liamsegura <liamdiegosegura@gmail.com>
2022-06-01 16:28:49 +0100

    animate on load

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ function headsortails(){
 }
 
 headsortails()
+
+
+// window.onload = function() {
+//     document.documentElement.classList.add('show-time');
+// };
+
+
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) {
+  
+    // wait until window is loaded - all images, styles-sheets, fonts, links, and other media assets
+    // you could also use addEventListener() instead
+    window.onload = function() {
+      
+       // OPTIONAL - waits til next tick render to run code (prevents running in the middle of render tick)
+       window.requestAnimationFrame(function() {
+      
+          // GSAP custom code goes here     
+          document.documentElement.classList.add('show-time');
+         
+       });
+      
+    };
+  
+});
